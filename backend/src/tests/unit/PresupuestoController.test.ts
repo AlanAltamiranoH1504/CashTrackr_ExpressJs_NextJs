ac2caa8563fb886b41c3ac2aad5447f3cabcd028
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-07-20 19:41:02 -0600

    test: add test for budget creation - add test for budget creation - add budget creation failure test

--- backend/src/tests/unit/PresupuestoController.test.ts
+++ backend/src/tests/unit/PresupuestoController.test.ts
@@ -1,12 +1,13 @@
 import {createRequest, createResponse} from "node-mocks-http";
 import {Presupuestos} from "../mocks/Presupuestos";
-import {findAll} from "../../controllers/PresupuestoController";
+import {findAll, save} from "../../controllers/PresupuestoController";
 import Presupuesto from "../../models/Presupuesto";
 
 jest.mock("../../models/Presupuesto", () => ({
     __esModule: true,
     default: {
-        findAll: jest.fn()
+        findAll: jest.fn(),
+        create: jest.fn()
     }
 }));
 
@@ -14,7 +15,7 @@ describe("PresupuestoController.findAll", () => {
     beforeEach(() => {
         (Presupuesto.findAll as jest.Mock).mockReset();
         (Presupuesto.findAll as jest.Mock).mockImplementation((options) => {
-            const presupuestosFiltro  = Presupuestos.filter(presupuesto => presupuesto.usuarioId === options.where.usuarioId);
+            const presupuestosFiltro = Presupuestos.filter(presupuesto => presupuesto.usuarioId === options.where.usuarioId);
             return Promise.resolve(presupuestosFiltro);
         });
     })
@@ -49,7 +50,7 @@ describe("PresupuestoController.findAll", () => {
             usuario: {id: 2}
         });
         const res = createResponse();
-        const presupuestosFiltro = Presupuestos.filter((presupuesto) =>{
+        const presupuestosFiltro = Presupuestos.filter((presupuesto) => {
             return presupuesto.usuarioId === req.usuario.id;
         });
         (Presupuesto.findAll as jest.Mock).mockResolvedValue(presupuestosFiltro);
@@ -76,5 +77,44 @@ describe("PresupuestoController.findAll", () => {
 
         const data = res._getJSONData();
         expect(res.statusCode).toBe(404);
+    });
+});
+
+describe("PresupuestoController.save", () => {
+    test("Prueba para la creacion y almacenamiento de un presupuesto", async () => {
+        const mockPresupuesto = {
+            save: jest.fn().mockResolvedValue(true)
+        };
+        (Presupuesto.create as jest.Mock).mockResolvedValue(mockPresupuesto);
+        const req = createRequest({
+            method: "POST",
+            url: "/presupuestos",
+            usuario: {id: 3},
+            body: {
+                nombre: "Presupuesto prueba",
+                monto: 2000
+            }
+        });
+        const res = createResponse();
+        await save(req, res);
+        const data = res._getJSONData();
+
+        expect(res.statusCode).toBe(201);
+        expect(data.msg).toBe("Presupuesto guardado correctamente.");
+    });
+
+    test("Prueba de error en la creacion y almacenamiento de un presupuesto", async () => {
+
+        (Presupuesto.create as jest.Mock).mockRejectedValue(new Error);
+        const req = createRequest({
+            method: "POST",
+            url: "/presupuestos",
+            usuario: {id: 3},
+        });
+        const res = createResponse();
+        await  save(req, res);
+        const data = res._getJSONData();
+        expect(res.statusCode).toBe(500);
+        expect(data.error).toBe("Error en creación de presupuesto.");
     });
 });
